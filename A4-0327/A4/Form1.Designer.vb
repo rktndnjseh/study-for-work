4a94f3e241d598f4e40677047415b2d9739701e7
rktndnjseh <park.castle3@gmail.com>
2025-03-28 16:13:34 +0900

    Add contents of A4_0327 to A4 folder

---
+++ A4-0327/A4/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button1.Location = New System.Drawing.Point(118, 131)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "지역별 수량 합계"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button2.Location = New System.Drawing.Point(335, 131)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(173, 40)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "지역별 금액 합계"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button3.Location = New System.Drawing.Point(543, 131)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(157, 40)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "지역별품목수량"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button4.Location = New System.Drawing.Point(118, 214)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(173, 43)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "지역별품목금액"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button5.Location = New System.Drawing.Point(335, 214)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(173, 43)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "통합품목수량"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("굴림", 14.0!)
+        Me.Button6.Location = New System.Drawing.Point(543, 214)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(157, 43)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "통합품목금액"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtFilePath.Font = New System.Drawing.Font("굴림", 11.0!)
+        Me.txtFilePath.Location = New System.Drawing.Point(54, 40)
+        Me.txtFilePath.Multiline = True
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.ReadOnly = True
+        Me.txtFilePath.Size = New System.Drawing.Size(100, 21)
+        Me.txtFilePath.TabIndex = 6
+        Me.txtFilePath.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents txtFilePath As TextBox
+End Class
---
+++ A4-0327/A4/Form1.vb
@@ -0,0 +1,314 @@
+﻿' EPPlus로 데이터 처리 후 Interop.Excel로 결과를 편집 가능한 워크북에 출력
+' 지연시간이 완전히 사라졌지만 몇몇 기능이 동작 안함
+Imports Microsoft.Office.Interop.Excel
+Imports System.IO
+Imports OfficeOpenXml
+Imports System.Reflection.Emit
+Imports System.ComponentModel
+
+Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+        ' EPPlus 라이선스 설정
+        OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial
+    End Sub
+    Dim filePath As String = ""
+    Dim exportPath As String = Path.Combine(
+        System.Windows.Forms.Application.StartupPath,
+  "통합분석결과.xlsx"
+)
+    'Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(System.Windows.Forms.Application.StartupPath).FullName).FullName).FullName).FullName,
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package As ExcelPackage = LoadOrCreatePackage()
+
+        DeleteSheetIfExists(package, "지역별 수량 합계")
+        Dim sheet = package.Workbook.Worksheets.Add("지역별 수량 합계")
+        sheet.Cells(1, 1).Value = "지역"
+        sheet.Cells(1, 2).Value = "수량"
+
+        Dim rowIndex As Integer = 2
+        Dim 전체합계 As Integer = 0
+
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                Dim 지역명 = s.Cells("B4").Text.Trim()
+                Dim 합계 = s.Cells(7, 4, 66, 4).Where(Function(c) IsNumeric(c.Value)).Sum(Function(c) Convert.ToInt32(c.Value))
+                If 합계 > 0 Then
+                    sheet.Cells(rowIndex, 1).Value = 지역명
+                    sheet.Cells(rowIndex, 2).Value = 합계
+                    전체합계 += 합계
+                    rowIndex += 1
+                End If
+            Next
+        End Using
+
+        sheet.Cells(rowIndex, 1).Value = "전체 합계"
+        sheet.Cells(rowIndex, 2).Value = 전체합계
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package = LoadOrCreatePackage()
+        DeleteSheetIfExists(package, "지역별 금액 합계")
+
+        Dim sheet = package.Workbook.Worksheets.Add("지역별 금액 합계")
+        sheet.Cells(1, 1).Value = "지역"
+        sheet.Cells(1, 2).Value = "금액"
+
+        Dim rowIndex As Integer = 2
+        Dim 전체합계 As Long = 0
+
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                Dim 지역명 = s.Cells("B4").Text.Trim()
+                Dim 합계 As Long = 0
+                For Each cell In s.Cells(7, 6, 66, 6)
+                    Dim val = cell.Text.Replace(",", "").Trim()
+                    Dim parsed As Long
+                    If Long.TryParse(val, parsed) Then 합계 += parsed
+                Next
+                If 합계 > 0 Then
+                    sheet.Cells(rowIndex, 1).Value = 지역명
+                    sheet.Cells(rowIndex, 2).Value = 합계
+                    전체합계 += 합계
+                    rowIndex += 1
+                End If
+            Next
+        End Using
+
+        sheet.Cells(rowIndex, 1).Value = "전체 금액 합계"
+        sheet.Cells(rowIndex, 2).Value = 전체합계
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package = LoadOrCreatePackage()
+
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                Dim 지역명 = s.Cells("B4").Text.Trim()
+                If String.IsNullOrWhiteSpace(지역명) Then Continue For
+
+                Dim baseName = 지역명 & " 수량"
+                Dim sheetName = baseName
+                Dim index = 1
+                While package.Workbook.Worksheets.Any(Function(ws) ws.Name = sheetName)
+                    sheetName = baseName & " (" & index & ")"
+                    index += 1
+                End While
+
+                Dim sheet = package.Workbook.Worksheets.Add(sheetName)
+                sheet.Cells(1, 1).Value = "품목"
+                sheet.Cells(1, 2).Value = "수량"
+
+                Dim dict As New Dictionary(Of String, Integer)
+                For i = 7 To 66
+                    Dim 품목 = s.Cells(i, 2).Text.Trim()
+                    Dim 수량 = s.Cells(i, 4).Text.Trim()
+                    If 품목 <> "" AndAlso IsNumeric(수량) Then
+                        If dict.ContainsKey(품목) Then dict(품목) += Convert.ToInt32(수량) Else dict.Add(품목, Convert.ToInt32(수량))
+                    End If
+                Next
+
+                Dim row = 2
+                For Each kvp In dict
+                    sheet.Cells(row, 1).Value = kvp.Key
+                    sheet.Cells(row, 2).Value = kvp.Value
+                    row += 1
+                Next
+            Next
+        End Using
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package = LoadOrCreatePackage()
+
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                Dim 지역명 = s.Cells("B4").Text.Trim()
+                If String.IsNullOrWhiteSpace(지역명) Then Continue For
+
+                Dim baseName = 지역명 & " 금액"
+                Dim sheetName = baseName
+                Dim index = 1
+                While package.Workbook.Worksheets.Any(Function(ws) ws.Name = sheetName)
+                    sheetName = baseName & " (" & index & ")"
+                    index += 1
+                End While
+
+                Dim sheet = package.Workbook.Worksheets.Add(sheetName)
+                sheet.Cells(1, 1).Value = "품목"
+                sheet.Cells(1, 2).Value = "금액"
+
+                Dim dict As New Dictionary(Of String, Decimal)
+                For i = 7 To 66
+                    Dim 품목 = s.Cells(i, 2).Text.Trim()
+                    Dim val = s.Cells(i, 6).Text.Replace(",", "").Trim()
+                    Dim 금액 As Decimal
+                    If 품목 <> "" AndAlso Decimal.TryParse(val, 금액) Then
+                        If dict.ContainsKey(품목) Then dict(품목) += 금액 Else dict.Add(품목, 금액)
+                    End If
+                Next
+
+                Dim row = 2
+                For Each kvp In dict
+                    sheet.Cells(row, 1).Value = kvp.Key
+                    sheet.Cells(row, 2).Value = kvp.Value
+                    row += 1
+                Next
+            Next
+        End Using
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package = LoadOrCreatePackage()
+        DeleteSheetIfExists(package, "통합 수량")
+
+        Dim sheet = package.Workbook.Worksheets.Add("통합 수량")
+        sheet.Cells(1, 1).Value = "품목"
+        sheet.Cells(1, 2).Value = "수량"
+
+        Dim dict As New Dictionary(Of String, Integer)
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                For i = 7 To 66
+                    Dim 품목 = s.Cells(i, 2).Text.Trim()
+                    Dim 수량 = s.Cells(i, 4).Text.Trim()
+                    If 품목 <> "" AndAlso IsNumeric(수량) Then
+                        If dict.ContainsKey(품목) Then dict(품목) += Convert.ToInt32(수량) Else dict.Add(품목, Convert.ToInt32(수량))
+                    End If
+                Next
+            Next
+        End Using
+
+        Dim row = 2
+        For Each kvp In dict
+            sheet.Cells(row, 1).Value = kvp.Key
+            sheet.Cells(row, 2).Value = kvp.Value
+            row += 1
+        Next
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If Not LoadExcelPath() Then Return
+
+        Dim package = LoadOrCreatePackage()
+        DeleteSheetIfExists(package, "통합 금액")
+
+        Dim sheet = package.Workbook.Worksheets.Add("통합 금액")
+        sheet.Cells(1, 1).Value = "품목"
+        sheet.Cells(1, 2).Value = "금액"
+
+        Dim dict As New Dictionary(Of String, Decimal)
+        Using origin = New ExcelPackage(New FileInfo(filePath))
+            For Each s In origin.Workbook.Worksheets
+                For i = 7 To 66
+                    Dim 품목 = s.Cells(i, 2).Text.Trim()
+                    Dim val = s.Cells(i, 6).Text.Replace(",", "").Trim()
+                    Dim 금액 As Decimal
+                    If 품목 <> "" AndAlso Decimal.TryParse(val, 금액) Then
+                        If dict.ContainsKey(품목) Then dict(품목) += 금액 Else dict.Add(품목, 금액)
+                    End If
+                Next
+            Next
+        End Using
+
+        Dim row = 2
+        For Each kvp In dict
+            sheet.Cells(row, 1).Value = kvp.Key
+            sheet.Cells(row, 2).Value = kvp.Value
+            row += 1
+        Next
+
+        SaveAndOpenPackage(package)
+    End Sub
+
+    Private Sub txtFilePath_TextChanged(sender As Object, e As EventArgs) Handles txtFilePath.TextChanged
+        Dim textSize As Size = TextRenderer.MeasureText(txtFilePath.Text, txtFilePath.Font)
+
+        ' 최소 너비와 최대 너비 설정 (폼을 넘어가지 않게)
+        Dim minWidth As Integer = 100
+        Dim maxWidth As Integer = Me.ClientSize.Width - txtFilePath.Left - 20 ' 오른쪽 여백 고려
+
+        ' 계산된 너비 + 약간의 여유 (스크롤 생기지 않게)
+        Dim newWidth As Integer = textSize.Width + 20
+
+        ' 범위 내에서만 설정
+        txtFilePath.Width = Math.Max(minWidth, Math.Min(newWidth, maxWidth))
+    End Sub
+
+    Private Function LoadExcelPath() As Boolean
+        If String.IsNullOrEmpty(filePath) Then
+            ' OpenFileDialog 사용
+            Using openFileDialog As New OpenFileDialog()
+                openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm"
+                openFileDialog.Title = "엑셀 파일 선택"
+
+                If openFileDialog.ShowDialog() = DialogResult.OK Then
+                    filePath = openFileDialog.FileName
+                    txtFilePath.Text = "사용하고 있는 파일 폴더 주소: " & filePath
+                Else
+                    MessageBox.Show("파일을 선택하지 않았습니다.")
+                    Return False
+                End If
+            End Using
+        End If
+
+        ' 파일 존재 여부 확인
+        If Not File.Exists(filePath) Then
+            MessageBox.Show("엑셀 파일이 존재하지 않습니다.")
+            Return False
+        End If
+        Return True
+    End Function
+    Private Function LoadOrCreatePackage() As ExcelPackage
+        If File.Exists(exportPath) Then
+            Return New ExcelPackage(New FileInfo(exportPath))
+        Else
+            Return New ExcelPackage()
+        End If
+    End Function
+
+    Private Sub SaveAndOpenPackage(package As ExcelPackage)
+        package.SaveAs(New FileInfo(exportPath))
+        Dim xlApp As New Application
+        xlApp.Workbooks.Open(exportPath)
+        xlApp.Visible = True
+    End Sub
+
+    Private Sub DeleteSheetIfExists(package As ExcelPackage, sheetName As String)
+        Dim existingSheet = package.Workbook.Worksheets.FirstOrDefault(Function(ws) ws.Name = sheetName)
+        If existingSheet IsNot Nothing Then
+            package.Workbook.Worksheets.Delete(existingSheet)
+        End If
+    End Sub
+
+End Class
